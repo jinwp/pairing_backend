445c1c051d616cd737291e0905ffe7c6a38049a1
boyeonp <dnflwlq20020416@gmail.com>
2025-07-15 15:18:53 +0000

    retry

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -2,9 +2,5 @@ import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-<<<<<<< HEAD
     love?: number;
-=======
-  love?: number;
->>>>>>> 20d1df791f0c7eaabe59f3535628c275a6669ea2
 }
