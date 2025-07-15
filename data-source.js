508e331fb51b7dbcbe7368bbdfbae899ac786e7f
boyeonp <dnflwlq20020416@gmail.com>
2025-07-15 17:27:00 +0000

    success revival

---
+++ data-source.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const typeorm_1 = require("typeorm");
+const dotenv = require("dotenv");
+dotenv.config();
+exports.default = new typeorm_1.DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: ['dist/**/*.entity.js'],
+    migrations: ['src/migrations/*.ts'],
+});
+//# sourceMappingURL=data-source.js.map
--- src/chatroom/chatroom.module.ts
+++ src/chatroom/chatroom.module.ts
@@ -7,8 +7,9 @@ import { UserModule } from '../user/user.module';
 import { User } from '../user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Chatroom, User]), UserModule],
+  imports: [TypeOrmModule.forFeature([Chatroom, User])],
   controllers: [ChatroomController],
   providers: [ChatroomService],
+  exports: [ChatroomService],
 })
 export class ChatroomModule {}
